9582d077bb4f9575302938d1479c057dcf9e024b
Jolan <krashbook@gmail.com>
2018-10-02 20:52:03 +0200

    awesome version of ex07

--- week1/day03/jolan/exo_07.rb
+++ week1/day03/jolan/exo_07.rb
@@ -32,28 +32,79 @@ def connect(password)
         print "> "
         tmp_password = gets.chomp
         if tmp_password == password
-            puts "You are now logged in (use CTRL+C to logout)"
-            i = 0
-            while i
-                i += 1
-            end
+            puts "You are now logged in"
+            puts "Please type -help to see command tools"
+            init()
         else
             puts "Password is wrong"
+            exit
         end
     elsif tmp == "n"
         puts "Sign in canceled"
+        exit
     else
         puts "Error"
+        exit
     end
 end
 
-password = define_password()
-check = verif_password(password)
-access = perform(password, check)
-
-if access == true
-    connect(password)
-else
-    puts "There is an error with your password"
-    puts "Please retry"
+def help()
+    puts "  -help -- use help to see command tools"
+    puts "  -show -- use show to print your password"
+    puts "  -chan -- use chan to change your password"
+    puts "  -exit -- use exit to logout"
+    print "\n"
 end
+
+def show()
+    puts "  -show -- Your actual password is #{@user_password}"
+    print "\n"
+end
+
+def chan()
+    puts "  -chan -- You make a request to change your password, you will be disconnected"
+    puts "      loading, please wait"
+    print "\n"
+    
+    password = define_password()
+    check = verif_password(password)
+    access = perform(password, check)
+
+    puts "  YOUR PASSWORD HAVE BEEN CHANGED"
+end
+
+def init()
+    i = 0
+    while i
+        if i == "-help"
+            help()
+        end
+        if i == "-show"
+            show()
+        end
+        if i == "-chan"
+            chan()
+        end
+        if i == "-exit"
+            puts "You are now disconnected"
+            exit
+        end
+        print "> "
+        i = gets.chomp
+    end
+end
+
+def launcher()
+    password = define_password()
+    check = verif_password(password)
+    access = perform(password, check)
+
+    if access == true
+        connect(password)
+    else
+        puts "There is an error with your password"
+        puts "Please retry"
+    end
+end
+
+launcher()
