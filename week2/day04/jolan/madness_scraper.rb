7d6bf1517ace63a710a584c75a3c0463f7ada648
Jolan <krashbook@gmail.com>
2018-10-11 16:05:23 +0200

    clean

--- week2/day04/jolan/madness_scraper.rb
+++ week2/day04/jolan/madness_scraper.rb
@@ -5,23 +5,6 @@ require 'pry'
 # Fetch and parse HTML document
 @doc = Nokogiri::HTML(open('http://annuaire-des-mairies.com/95/vaureal.html'))
 
-def example()
-    puts "### Search for nodes by css"
-    doc.css('txt-primary tr-last', 'td').each do |link|
-    puts link.content
-    end
-
-    puts "### Search for nodes by xpath"
-    doc.xpath('//txt-primary//tr-last', '//td//h2').each do |link|
-    puts link.content
-    end
-
-    puts "### Or mix and match."
-    doc.search('nav ul.menu li a', '//article//h2').each do |link|
-    puts link.content
-    end
-end
-
 def parse()
     content = Array.new
     @doc.search('txt-primary tr-last', 'td').each do | link |
