57972d548d2fd7d8969031e2c7e2124b8d52f197
Jolan <krashbook@gmail.com>
2018-10-17 22:33:43 +0200

    test on csv

--- week3/day03/lib/manage_csv.rb
+++ week3/day03/lib/manage_csv.rb
@@ -7,9 +7,10 @@ class ManageCsv
     end
 
     def create(hash)
-        CSV.open('/Users/c0dex/Laboratory/main-thp/git-thp/week3/day03/db/townhalls_mail_data.csv', 'w') do | csv |
+        CSV.open('/Users/c0dex/Laboratory/main-thp/git-thp/week3/day03/db/townhalls_mail_data.csv', 'wb') do |csv|
             hash.each do | key, value | 
-                csv << [ key, value, "\n" ]
+                csv << [ key ]
+                csv << [ value ]
             end
         end
     end
