dcea59a398160462c31999547b69a52a1cf116a0
Jolan <krashbook@gmail.com>
2018-10-22 21:36:50 +0200

    test

--- week4/day01/lib/controller.rb
+++ week4/day01/lib/controller.rb
@@ -2,14 +2,17 @@ require 'gossip'
 
 class ApplicationController < Sinatra::Base
   get '/' do
+    puts "HELLO"
     erb :index, locals: { gossips: Gossip.all }
   end
 
   get '/gossips/new/' do
+    puts "JE SUIS DANS LA ROUTE GOSSIP NEW"
     erb :new_gossip
   end
 
   post '/gossips/new/' do
+    puts "JE FAIS UNE REQUETE POST DANS LA ROUT GOSSIP NEW"
     Gossip.new(params['gossip_author'], params['gossip_content']).save()
     redirect '/'
   end
